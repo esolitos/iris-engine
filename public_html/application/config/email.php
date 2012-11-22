54ff09dd2eb0b607f2e7fc072ea2cbddfc4c1cf7
esolitos <esolitos@bba480f2-e8c1-40e4-ad25-c25a4d800b06>
2012-11-22 01:20:14 +0000

    Aggiornate Impostazioni email.
    git-svn-id: svn+ssh://esolitos.com/var/svn-repos/iris-dynengine/trunk@100 bba480f2-e8c1-40e4-ad25-c25a4d800b06

--- public_html/application/config/email.php
+++ public_html/application/config/email.php
@@ -20,23 +20,23 @@ switch (TRUE) {
 
 	case (strstr($_SERVER['HTTP_HOST'],'test')):
 	default:
-		$config = array(
+		$config = Array(
 			'useragent'		=>	'irislogin',
 			'crlf'			=> "\r\n",
 			'newline'		=> "\r\n",
 			'charset'		=> 'utf-8',
 			'mailtype'		=> 'html',
-
-			'protocol'		=> 'smtp',
-			'smtp_host'		=> 'mail.irislogin.it',
-			'smtp_port'		=> '25',
-			'smtp_timeout'	=> '10',
-			'smtp_user'		=> 'no-reply@irislogin.it',
-			'smtp_pass'		=> 'IRISLOGIN2012-no-login',
+			
+		    'protocol' => 'smtp',
+			'useragent'		=>	'irislogin',
+		    'smtp_host' => 'ssl://smtp.googlemail.com',
+		    'smtp_port' => 465,
+		    'smtp_user' => 'no-reply@irislogin.it',
+		    'smtp_pass' => 'IRISLOGIN2012-no-login',
 
 			'bcc_batch_mode'=> TRUE,
 			'bcc_batch_size'=> '100'
-			);
+		);
 		break;
 }
 
